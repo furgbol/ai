9b17511d9438c301d7cb4b3ec955bac507d841bf
Miguel dos Santos <miguelmartinsdossantos@gmail.com>
2020-03-10 21:09:39 -0300

    control/navigation: First path planning code done

---
+++ control/navigation/casteljau.go
@@ -0,0 +1,76 @@
+package navigation
+
+import (
+	"math"
+
+	"github.com/furgbol/ai/model"
+)
+
+// CasteljauPathPlanner - This type implements the PathPlanner interface. It plans the path based on Casteljau's algorithm that uses Bézier curves.
+type CasteljauPathPlanner struct {
+	pathPointsNumber int
+	usedPointsNumber int
+	distanceFactor 	 int
+}
+
+// NewCasteljauPathPlanner creates an instance of a Casteljau Path Planner
+func NewCasteljauPathPlanner(numberOfPathPoints, numberOfUsedPoints, distanceFactor int) *CasteljauPathPlanner {
+	return &CasteljauPathPlanner{,
+		pathPointsNumber: numberOfPathPoints, 
+		usedPointsNumber: numberOfUsedPoints,
+		distanceFactor: distanceFactor,
+	}
+}
+
+// PlanPath is the function that calculate the path
+func (pathPlanner CasteljauPathPlanner) PlanPath(initialPose, targetPose model.Pose) {
+	distance := calculateDistanceBetweenTwoPoints(,
+		model.Position2D{,
+			X: initialPose.X,
+			Y: initialPose.Y,
+		},
+
+		model.Position2D{,
+			X: targetPose.X,
+			Y: targetPose.Y,
+		},
+	)
+
+	firstControlPoint, secondControlPoint := pathPlanner.generateControlPoints(initialPose, targetPose, distance)
+
+	path := pathPlanner.getPath(initialPose, targetPose, firstControlPoint, secondControlPoint)
+}
+
+func calculateDistanceBetweenTwoPoints(firstPoint, secondPoint model.Position2D) float64 {
+	return math.Sqrt(math.Pow((secondPoint.X - firstPoint.X), 2) + math.Pow((secondPoint.Y - firstPoint.Y), 2)) 
+}
+
+func (pathPlanner CasteljauPathPlanner) generateControlPoints(initialPose, targetPose model.Pose, distance float64) (model.Position2D, model.Position2D) {
+	firstPoint := model.Position2D{,
+		math.Cos(initialPose.Orientation) * (distance / distanceFactor) + initialPose.X,
+		math.Sin(initialPose.Orientation) * (distance / distanceFactor) + initialPose.Y,
+	}
+
+	secondPoint := model.Position2D{,
+		math.Cos(targetPose.Orientation + 180) * (distance / distanceFactor) + targetPose.X,
+		math.Sin(targetPose.Orientation + 180) * (distance / distanceFactor) + targetPose.Y,
+	}
+
+	return firstPoint, secondPoint
+}
+
+func (pathPlanner CasteljauPathPlanner) getPath(initialPose, targetPose model.Pose, firstControlPoint, secondControlPoint model.Position2D) Path {
+	path := NewPath(pathPlanner.numberOfUsedPoints)
+
+	factor := 1 / pathPlanner.numberOfPathPoints
+	t := 0
+	for i := 0; i < pathPlanner.numberOfUsedPoints; i++ {
+		t += factor
+		path[i] = model.Position2D{,
+			math.Pow((1 - t), 3) * initialPose.X + 3 * t * math.Pow((1 - t), 2) * firstControlPoint.X + 3 * math.Pow(t, 2) * (1 - t) * secondControlPoint.X + math.Pow(t, 3) * targetPose.X,
+			math.Pow((1 - t), 3) * initialPose.Y + 3 * t * math.Pow((1 - t), 2) * firstControlPoint.Y + 3 * math.Pow(t, 2) * (1 - t) * secondControlPoint.Y + math.Pow(t, 3) * targetPose.Y,
+		}
+	}
+
+	return path
+}
---
+++ control/navigation/path.go
@@ -0,0 +1,11 @@
+package navigation
+
+import "github.com/furgbol/ai/model"
+
+// Path is the data type to store a path
+type Path []model.Position2D
+
+func NewPath(numberOfPoints int) Path {
+	path := make(Path, numberOfPoints, numberOfPoints)
+	return path
+}
