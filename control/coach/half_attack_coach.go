5e33d2b8d11c7ea6f87d0b15adb73e8147c5aca0
brunolobell <44982789+brunolobell@users.noreply.github.com>
2020-04-08 13:12:38 -0300

    Update functions
    Update the functions: NewHalfAttackCoach and GetRoles

--- control/coach/half_attack_coach.go
+++ control/coach/half_attack_coach.go
@@ -2,15 +2,6 @@ package coach
 
 import "github.com/furgbol/ai/control/evaluation"
 
-const (
-	// Position of robot in goal
-	Goalkeeper = "inGoal"
-	// Position of robot in attack
-	Attacker = "inAttack"
-	// Position of robot in defense
-	Defender = "inDefense"
-)
-
 // HalfAttackCoach - This type implement the Coach interface.
 type HalfAttackCoach struct {
 	Index map[int]string
@@ -18,6 +9,9 @@ type HalfAttackCoach struct {
 
 // NewHalfAttackCoach creates an instance of a Half Attack Coach  
 func NewHalfAttackCoach(index map[int]string) *HalfAttackCoach {
+	index[1] = "Goalkeeper"
+	index[2] = "Centerback"
+	index[3] = "striker"
 	return &HalfAttackCoach{
 		Index: index,
 	} 
@@ -25,11 +19,5 @@ func NewHalfAttackCoach(index map[int]string) *HalfAttackCoach {
 
 // GetRoles is the function that return the role of robot
 func (coach HalfAttackCoach) GetRoles(evaluation evaluation.Evaluation) map[int]string {
-	var NewCoach map[int]string
-	Robots := evaluation.word.Team 
-	for i := 0; i < len(Robots); i+++ {
-		robot := Robots[i]
-		NewCoach[robots.ID], _ := coach[i]  
-	}
-	return NewCoach
+	return coach.Index
 }
