1f5e3204d4536d0d945467703bb2a6c8a61a2f19
Thiago <thhiaggo1@hotmail.com>
2020-04-09 17:56:28 -0300

    Update by thiago

--- control/evaluation/evaluation.go
+++ control/evaluation/evaluation.go
@@ -2,7 +2,23 @@ package evaluation
 
 import "github.com/furgbol/ai/model"
 
-// Evaluation - type that stores the evaluation of the world
+// Evaluation - type that stores the evaluation of the model world
 type Evaluation struct {
 	world model.World
+}
+
+// DummyEvaluator - type that stores the struct evaluation 
+type DummyEvaluator struct {
+	eval Evaluation
+}
+
+// DummyEvaluator creates an instance of a DummyEvaluator  
+func NewDummyEvaluator() *DummyEvaluator {
+	return &DummyEvaluator{}
+}
+
+// Evaluate is the function that return the evaluation with model world
+func (duEvaluator *DummyEvaluator) Evaluate(world model.World) Evaluation {
+	duEvaluator.Evaluation.World = world
+	return duEvaluator.Evaluation
 }
